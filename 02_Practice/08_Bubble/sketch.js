ab0ec02120a7e555d8b8e6f98dba88649c57d8f9
Sean Kirkpatrick <mbp@SKMacBook-2.local>
2018-07-22 23:11:30 -0400

    D. Shiffman 7.7

--- 02_Practice/08_Bubble/sketch.js
+++ 02_Practice/08_Bubble/sketch.js
@@ -1,26 +1,39 @@
-let bubble1;
-let bubble2;
+let bubbles = [];
 
 function setup() {
 	createCanvas(600, 400);
-	bubble1 = new Bubble(200, 200);
-	bubble2 = new Bubble(400, 200, 100);
+	for (let i = 0; i < 30; i++){
+		let x = random(width);
+		let y = random(height);
+		let r = random(10,50);
+		bubbles[i] = new Bubble(x, y, r);
+	}
 
 }
 
 function draw() {
 	background(0);
 
-	if (bubble1.intersects(bubble2)) {
-		background(200,0,100);
+	for (let b of bubbles){
+		b.show();
+		b.move();
+		let overlapping = false;
+		for (let other of bubbles){
+			if (b !== other && b.intersects(other)) {
+				overlapping = true;
+			}
+		}
+		if (overlapping) {
+			b.changeColor(255);
+		} else {
+			b.changeColor(0);
+		}
 	}
 
-	bubble1.show();
-	bubble2.show();
-	bubble1.move();
-	// bubble2.move();
-	bubble2.x = mouseX;
-	bubble2.y = mouseY;
+	// for (let i = 0; i < bubbles.length; i++) {
+	// 	bubbles[i].show();
+	// 	bubbles[i].move();
+	// }
 }
 
 class Bubble {
@@ -33,13 +46,7 @@ class Bubble {
 
 	intersects(other) {
 		let d = dist(this.x, this.y, other.x, other.y);
-		return (d < bubble1.r + bubble2.r);
-
-		// if (d < bubble1.r + bubble2.r) {
-		// 	return true;
-		// } else {
-		// 	return false;
-		// }
+		return (d < this.r + other.r);
 	}
 
 	changeColor(bright) {
@@ -56,8 +63,8 @@ class Bubble {
 	}
 
 	move() {
-		this.x = this.x + random(-5, 5);
-		this.y = this.y + random(-5, 5);
+		this.x = this.x + random(-1, 1);
+		this.y = this.y + random(-1, 1);
 	}
 
 	show() {
