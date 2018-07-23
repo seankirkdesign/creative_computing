a0ca64a09da2b6507cf3a67e32e303c46506cf12
Sean Kirkpatrick <mbp@SKMacBook-2.local>
2018-07-22 22:07:01 -0400

    Daniel Shiffman 7.6

---
+++ 02_Practice/08_Bubble/sketch.js
@@ -0,0 +1,69 @@
+let bubble1;
+let bubble2;
+
+function setup() {
+	createCanvas(600, 400);
+	bubble1 = new Bubble(200, 200);
+	bubble2 = new Bubble(400, 200, 100);
+
+}
+
+function draw() {
+	background(0);
+
+	if (bubble1.intersects(bubble2)) {
+		background(200,0,100);
+	}
+
+	bubble1.show();
+	bubble2.show();
+	bubble1.move();
+	// bubble2.move();
+	bubble2.x = mouseX;
+	bubble2.y = mouseY;
+}
+
+class Bubble {
+	constructor(x, y, r = 50) {
+		this.x = x;
+		this.y = y;
+		this.r = r;
+		this.brightness = 0;
+	}
+
+	intersects(other) {
+		let d = dist(this.x, this.y, other.x, other.y);
+		return (d < bubble1.r + bubble2.r);
+
+		// if (d < bubble1.r + bubble2.r) {
+		// 	return true;
+		// } else {
+		// 	return false;
+		// }
+	}
+
+	changeColor(bright) {
+		this.brightness = bright;
+	}
+
+	contains(){
+		let d = dist(px, py, this.x, this.y)
+		if (d < this.r) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	move() {
+		this.x = this.x + random(-5, 5);
+		this.y = this.y + random(-5, 5);
+	}
+
+	show() {
+		stroke(255);
+		strokeWeight(4);
+		fill(this.brightness, 125);
+		ellipse(this.x, this.y, this.r * 2);
+	}
+}
