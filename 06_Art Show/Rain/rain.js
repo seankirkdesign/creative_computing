53b248911401142a9265c8fdda46175b3a0b69aa
sean kirkpatrick <seungkim94@gmail.com>
2018-08-10 13:48:19 -0400

    Rain Art Show Rotation

---
+++ 06_Art Show/Rain/rain.js
@@ -0,0 +1,29 @@
+class Rain {
+  constructor(x, y, yspeed){
+    this.x = random(-width);
+    this.y = random(-height);
+    this.yspeed = random(2,4);
+  }
+
+  fall(){
+    this.y += this.yspeed;
+
+    if (this.y > height + 500){
+      this.y = random(-height);
+    }
+  }
+
+  show(){
+    push();
+    fill(255);
+    rotate(90);
+    textSize(20);
+    textAlign(RIGHT);
+    textFont('IBM Plex Mono');
+    text('look at all these things', this.y, this.x);
+    // line(this.x, this.y, this.x, this.y + 10)
+    pop();
+  }
+
+
+}
