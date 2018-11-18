2b7b538e2cddff4420d58ff219bc78528f1f2df4
Sean Kirkpatrick <mbp@SKMacBook-2.local>
2018-11-17 21:51:11 -0800

    Website + Completed Projects

---
+++ 01_Complete Projects/01_TYPE/sketch.js
@@ -0,0 +1,43 @@
+var charObjs = [];
+var chars = "type:";
+var nextCharXPos = 0;
+var charSize = 30;
+var value = 245;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  smooth();
+  textFont('COURIER');
+  for (var i=0; i<chars.length; i++) {
+    charObjs[i] = new MovingChar(chars[i], nextCharXPos, height/2, charSize, 0.07);
+    nextCharXPos += charSize;
+  }
+}
+
+function draw() {
+  background(value);
+  push();
+  translate(width/2 - nextCharXPos, 0);
+  for (var i=0; i<charObjs.length; i++) {
+    charObjs[i].move();
+    charObjs[i].bounce(width, height);
+    charObjs[i].draw();
+  }
+  pop();
+}
+
+function keyPressed() {
+
+  var newChar = new MovingChar(key, nextCharXPos, height/2, size, 0.07);
+  charObjs.push(newChar);
+  nextCharXPos += charSize;
+
+  /*
+  if (value === 0) {
+    value = 230;
+  } else {
+    value = 0;
+  }
+  */
+}
---
+++ 01_Complete Projects/02_SUPER_POWER/sketch.js
@@ -0,0 +1,108 @@
+var ps
+
+var bgCircle ={
+  x:1,
+  xSpeed:10.5,
+  y:2,
+  ySpeed:14,
+  size:20,
+};
+
+var circle = {
+  size:25
+}
+
+var square ={
+  x:0,
+  y:0,
+  size:50,
+};
+
+var mousePositions = [];
+var numOfMousePos = 15;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  ps = new ParticleSystem();
+
+}
+
+function draw() {
+  // add 10 previous mouse positions.
+  mousePositions.unshift(createVector(mouseX, mouseY));
+  if(mousePositions.length > numOfMousePos) {
+      mousePositions.pop();
+  }
+
+  r = map(bgCircle.x, 0, windowWidth, 0, 255);
+  g = map(bgCircle.y, 0, windowHeight, 0, 255);
+  b = map(mouseY, 0, 500, 0, 255);
+  background(r, g, 150);
+
+  if (mouseIsPressed){
+    noFill();
+    circle.size = 55;
+  } else {
+    background(35);
+    circle.size = 15;
+  }
+  //This makes circle bounce
+  bgCircle.x += bgCircle.xSpeed
+  bgCircle.y += bgCircle.ySpeed
+
+  if (bgCircle.x < 0 || bgCircle.x > width) {
+    bgCircle.xSpeed = bgCircle.xSpeed * -1;
+  }
+
+  if (bgCircle.y < 0 || bgCircle.y > height) {
+    bgCircle.ySpeed = bgCircle.ySpeed * -1;
+  }
+
+  textSize(15);
+  textAlign(CENTER);
+  text("Click to be a super hero.", width / 2 , height / 2);
+  //bouncing circle
+  noFill();
+  noStroke();
+  ellipse(bgCircle.x, bgCircle.y, bgCircle.size, bgCircle.size);
+
+  for (square.x = 0; square.x <= width; square.x += square.size ){
+    for (square.y = 0; square.y <= height; square.y += square.size ){
+      for (var i = 0; i < mousePositions.length; i++) {
+        var mPos = mousePositions[i];
+        if (square.x - 0 < mPos.x &&
+            square.x + 50 > mPos.x &&
+            square.y - 0 < mPos.y &&
+            square.y + 50 > mPos.y) {
+          fill(r, g, 150, (1-i/mousePositions.length) * 255);
+          rect (square.x, square.y, square.size, square.size);
+        }
+      }
+    }
+  }
+
+  if (mouseIsPressed){
+    noFill();
+    circle.size = 55;
+    push();
+    fill(255);
+    textSize(15);
+    textAlign(CENTER);
+    text("Isn't it awesome?", width / 2 , height / 2)
+    pop();
+  } else {
+    circle.size = 15;
+  }
+
+  //cursor
+  fill(255);
+  ellipse(mouseX,mouseY,circle.size, circle.size);
+  noCursor();
+
+  noStroke();
+  ps.update();
+  ps.display();
+  if (isMousePressed){
+    ps.addParticles(20, createVector(mouseX, mouseY));
+  }
+}
