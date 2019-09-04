beb0024b0832aa99ff0419b8082981ade94801de
Sean <skirkpatrick@sva.edu>
2019-09-04 16:50:10 -0700

    update

--- 01_Complete/05_Loading-Balls/mover.js
+++ 01_Complete/05_Loading-Balls/mover.js
@@ -3,8 +3,7 @@ class Mover {
      this.loc = location;
      this.vel = createVector(random(-0.5, 0.5), random(-0.5, 0.5));
      this.acc = createVector(0, 0);
-     this.gra = 0, 0.5;
-     // this.gra = createVector(random(-0.015, 0.015), random(-0.015, 0.015));
+     this.gra = createVector(random(-0.015, 0.015), random(-0.015, 0.015));
      this.size = 50;
      this.color = random(70, 255);
 
@@ -38,25 +37,25 @@ class Mover {
   // }
 
   checkBound() {
-    // if (this.loc.x > width) {
-    //   this.loc.x = 0;
-    // } else if (this.loc.x < 0) {
-    //   this.loc.x = width;
+    if (this.loc.x > width) {
+      this.loc.x = 0;
+    } else if (this.loc.x < 0) {
+      this.loc.x = width;
+    }
+
+    if (this.loc.y > height) {
+      this.loc.y = 0;
+    } else if (this.loc.y < 0) {
+      this.loc.y = height;
+    }
+
+    // if (this.loc.x + this.size/2 > width || this.loc.x - this.size/2 < 0) {
+    //   this.vel.x *= -1;
     // }
     //
-    // if (this.loc.y > height) {
-    //   this.loc.y = 0;
-    // } else if (this.loc.y < 0) {
-    //   this.loc.y = height;
+    // if (this.loc.y + this.size/2 > height || this.loc.y - this.size/2 < 0) {
+    //   this.vel.y *= -1;
     // }
-
-    if (this.loc.x + this.size/2 > width || this.loc.x - this.size/2 < 0) {
-      this.vel.x *= -1;
-    }
-
-    if (this.loc.y + this.size/2 > height || this.loc.y - this.size/2 < 0) {
-      this.vel.y *= -1;
-    }
   }
 
   displayLover() {
